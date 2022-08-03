7a37c386990d77486ed957ff671b5eec1464938f
Abhisek Singh <63722822+abhisek6977@users.noreply.github.com>
2022-08-04 03:30:14 +0530

    dataview with string html

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,13 +1,20 @@
 '''
 To render html web pages
 '''
+import random
 from django.http import HttpResponse
 
-HTML_STRING = """
-<h1> hello world</h1>
-"""
 
 
 def home_view(request):
 
+    name = "abhisek"
+    number = random.randint(10, 1233123)
+    H1_STRING = f"""
+        <h1> hello {name} - {number}!</h1>
+        """
+    P_STRING = f"""
+        <p> hi {name} - {number}!</p>
+        """
+    HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
