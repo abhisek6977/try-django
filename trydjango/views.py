4e1345b3e946fb77420a150df5d33d84d244a8f8
Abhisek Singh <63722822+abhisek6977@users.noreply.github.com>
2022-08-04 04:24:22 +0530

    dynamic pull of data from db to content

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,7 +3,7 @@ To render html web pages
 '''
 import random
 from django.http import HttpResponse
-
+from articals.models import Artical
 
 
 def home_view(request):
@@ -11,12 +11,14 @@ def home_view(request):
     #from database
 
     name = "abhisek"
-    number = random.randint(10, 1233123)
+    random_id = random.randint(1, 3)
+    artical_obj = Artical.objects.get(id=random_id)
+
     H1_STRING = f"""
-        <h1> hello {name} - {number}!</h1>
+        <h1> hello {artical_obj.title} (id: {artical_obj.id})!</h1>
         """
     P_STRING = f"""
-        <p> hi {name} - {number}!</p>
+        <p> hi {artical_obj.content}!</p>
         """
     HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
