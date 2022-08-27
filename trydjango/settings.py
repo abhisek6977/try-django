5886b901bf24cc783fd8c394c6a0b3b2d53b2f60
Abhisek Singh <abhiseksingh@Abhiseks-MacBook-Air.local>
2022-08-27 12:06:08 +0530

    ordered and un ordered list

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print('BASE_DIR', BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            '/Users/abhiseksingh/Desktop/projects/dev/try-django/templates'
+            BASE_DIR / 'templates',
+            # '/Users/abhiseksingh/Desktop/projects/dev/try-django/templates'
             ],
         'APP_DIRS': True,
         'OPTIONS': {
--- trydjango/views.py
+++ trydjango/views.py
@@ -14,8 +14,12 @@ def home_view(request):
     name = "abhisek"
     random_id = random.randint(1, 3)
     artical_obj = Artical.objects.get(id=random_id)
+    artical_qs = Artical.objects.all()
+
+    my_list = artical_qs #[123, 12 , 34 , 65, 76]
 
     context = {
+        'object_list': my_list,
         'title': artical_obj.title,
         'id': artical_obj.id,
         'content': artical_obj.content
